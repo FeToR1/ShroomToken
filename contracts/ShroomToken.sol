b47a0f728ca40c93c1f7950264af2b401c0c4de4
Fedor Kutergin <fedornigretuk@gmail.com>
2026-03-04 01:40:19 +0300

    upd

--- contracts/ShroomToken.sol
+++ contracts/ShroomToken.sol
@@ -5,11 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ShroomToken is ERC20, Ownable {
-    constructor() 
-        ERC20("ShroomToken", "SHRM")
-        Ownable(msg.sender)
-    {}
-    
+    constructor() ERC20("ShroomToken", "SHRM") Ownable(msg.sender) {}
+
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
--- scripts/deploy_contract.py
+++ scripts/deploy_contract.py
@@ -1,6 +1,6 @@
 import json
-from web3 import Web3
 import os
+from web3 import Web3
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,27 +11,28 @@ INFURA_URL = os.getenv('INFURA_URL')
 
 w3 = Web3(Web3.HTTPProvider(INFURA_URL))
 
-
+print(w3.from_wei(w3.eth.get_balance(ACCOUNT_ADDRESS), 'ether'))
 with open('ShroomToken.abi.json') as f:
     abi = json.load(f)
-
 with open('ShroomToken.bin-runtime') as f:
     bytecode = f.read()
 
 ShroomToken = w3.eth.contract(abi=abi, bytecode=bytecode)
 
 nonce = w3.eth.get_transaction_count(ACCOUNT_ADDRESS)
+
 transaction = ShroomToken.constructor().build_transaction({
     'from': ACCOUNT_ADDRESS,
     'nonce': nonce,
     'gas': 2_500_000,
-    'gasPrice': w3.to_wei('10', 'gwei'),
+    'gasPrice': w3.to_wei('5', 'gwei'),
+    'chainId': 11155111,
 })
 
 signed_tx = w3.eth.account.sign_transaction(transaction, PRIVATE_KEY)
 
 tx_hash = w3.eth.send_raw_transaction(signed_tx.raw_transaction)
-print(f"Транзакция отправлена! hash: {w3.to_hex(tx_hash)}")
+print(f"Транзакция отправлена! hash: {tx_hash.hex()}")
 
 tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
 print(f"Контракт задеплоен по адресу: {tx_receipt.contractAddress}")
